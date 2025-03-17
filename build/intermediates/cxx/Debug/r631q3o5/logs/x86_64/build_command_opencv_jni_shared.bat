f3740d36d94057dcb4cbe7bf67cb9054371dba51
Georgios Nicolaou <gnicolaou92@gmail.com>
2025-03-17 16:07:47 +0200

    first commit

---
+++ build/intermediates/cxx/Debug/r631q3o5/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\nicol\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\OpenCV-android-sdk\\sdk\\.cxx\\Debug\\r631q3o5\\x86_64" ^
+  opencv_jni_shared
